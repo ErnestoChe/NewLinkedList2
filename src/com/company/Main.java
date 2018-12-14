9f29d911113baef55fa0289f7b5e59b32943fb78
ErnestoChe <ernestfilippov@mail.ru>
2018-12-14 14:09:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,159 @@
+package com.company;
+
+import java.util.ArrayList;
+
+class LinkedList2
+{
+    public Node head;
+    public Node tail;
+
+    public LinkedList2()
+    {
+        head = null;
+        tail = null;
+    }
+
+    public void addInTail(Node _item)
+    {
+        if (head == null) {
+            this.head = _item;
+            this.head.next = null;
+            this.head.prev = null;
+        } else {
+            this.tail.next = _item;
+            _item.prev = tail;
+        }
+        this.tail = _item;
+    }
+
+    public void addInHead(Node _item){
+        _item.next = head;
+        _item.prev = null;
+        if(head!=null){
+            head.prev = _item;
+        }
+        head = _item;
+    }//tested
+    public Node find(int _value)
+    {
+        Node tmp = head;
+        while(tmp!= null && tmp.value != _value){
+            tmp = tmp.next;
+        }
+        return tmp;
+    }
+
+    public ArrayList<Node> findAll(int _value)
+    {
+        ArrayList<Node> nodes = new ArrayList<Node>();
+        Node tmp = head;
+        while(tmp != null){
+            if(tmp.value == _value){
+                nodes.add(tmp);
+            }
+            tmp = tmp.next;
+        }
+        return nodes;
+    }
+
+    public boolean remove(int _value)
+    {
+        boolean flag = false;
+        if(head == null){
+            flag = false;
+        }else if(head == tail){
+            head = null;
+            tail = null;
+            flag = true;
+        }else if(tail.value == _value){
+            tail = tail.prev;
+            tail.next = null;
+            flag = true;
+        }else if(head.value == _value){
+            head = head.next;
+            flag = true;
+        }else {
+            Node key = find(_value);
+            if(key == null){
+                flag = false;
+            }else{
+                key.prev.next = key.next;
+                key.next.prev = key.prev;
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+    public void removeAll(int _value)
+    {
+        if(head.next == tail){
+            remove(_value);
+        }else{
+            boolean flag = remove(_value);
+            if(flag)
+                while(flag){
+                    flag = remove(_value);
+                }
+        }
+    }
+
+    public void clear()
+    {
+        head = null;
+        tail = null;
+    }
+
+    public int count()
+    {
+        Node temp = head;
+        int count = 0;
+        while (temp != null)
+        {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public void insertAfter(Node _nodeAfter, Node _nodeToInsert)
+    {
+        if(head != null && _nodeAfter == null){
+            addInTail(_nodeToInsert);
+        }else
+        if(tail == _nodeAfter){
+            addInTail(_nodeToInsert);
+        }else{
+            _nodeToInsert.next = _nodeAfter.next;
+            _nodeAfter.next.prev = _nodeToInsert;
+            _nodeAfter.next = _nodeToInsert;
+            _nodeToInsert.prev = _nodeAfter;
+        }
+    }
+    public void log(){
+        Node tmp = head;
+        if(head == null){
+            System.out.println("empty list");
+        }else{
+            while(tmp != null){
+                System.out.print(tmp.value+" ");
+                tmp = tmp.next;
+            }
+        }
+        System.out.println();
+    }
+}
+
+class Node
+{
+    public int value;
+    public Node next;
+    public Node prev;
+
+    public Node(int _value)
+    {
+        value = _value;
+        next = null;
+        prev = null;
+    }
+}
---
+++ src/com/company/Test.java
@@ -0,0 +1,384 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void testCount(){
+        LinkedList2 list = new LinkedList2();
+        for(int i = 0; i<5; i++){
+            list.addInTail(new Node(i+1));
+        }
+        int success = 0, fail = 0;
+
+        if(list.count() == 5){
+            success++;
+            System.out.println("count 5 success");
+        }else {
+            fail++;
+            System.out.println("count 5 fail");
+        }
+
+
+        list.clear();
+        list.addInTail(new Node(1));
+        if(list.count() == 1){
+            success++;
+            System.out.println("count 1 success");
+        }else {
+            fail++;
+            System.out.println("count 1 fail");
+        }
+
+        list.clear();
+        if(list.count() == 0){
+            success++;
+            System.out.println("count 0 success");
+        }else {
+            fail++;
+            System.out.println("count 0 fail");
+        }
+
+        System.out.println(success+" success");
+        System.out.println(fail+" fail");
+    }
+    public static void testFind(){
+        LinkedList2 list = new LinkedList2();
+        for(int i = 0; i<5; i++){
+            list.addInTail(new Node(i+1));
+        }
+        int success = 0, fail = 0;
+        Node found = list.find(9);
+        if(found == null){
+            success++;
+            System.out.println("not existing not found");
+        }else {
+            fail++;
+            System.out.println("non existing found somehow?????");
+        }
+        //finding non existing
+
+        Node found1 = list.find(1);
+        if(found1.value == 1){
+            success++;
+            System.out.println("found 1");
+        }else {
+            fail++;
+            System.out.println("didnt find 1");
+        }
+        //found first in list
+
+        Node found5 = list.find(5);
+        if(found5.value == 5){
+            success++;
+            System.out.println("found 5");
+        }else {
+            fail++;
+            System.out.println("didnt find 5");
+        }
+        //found last in list
+
+        Node found3 = list.find(3);
+        if(found3.value == 3 && found3.next == list.tail.prev && list.head.next == found3.prev){
+            success++;
+            System.out.println("found 3");
+        }else {
+            fail++;
+            System.out.println("didnt find 3");
+        }
+        //found in the middle of list
+
+        System.out.println(success+" success");
+        System.out.println(fail+" fail");
+    }
+    public static void testRemove(){
+        LinkedList2 list = new LinkedList2();
+        for(int i = 0; i<5; i++){
+            list.addInTail(new Node(i+1));
+        }
+        int success = 0, fail = 0;
+        boolean check = list.remove(9);
+        if(!check){
+            success++;
+            System.out.println("coudlnt delete non existing element(9)");
+        }else{
+            fail++;
+            System.out.println("somehow deleted non existing element");
+        }
+
+        check = list.remove(1);
+        if(check){
+            success++;
+            System.out.println("deleted first element");
+        }else{
+            fail++;
+            System.out.println("coudlnt delet first element");
+        }
+
+        check = list.remove(5);
+        if(check){
+            success++;
+            System.out.println("deleted last element");
+        }else{
+            fail++;
+            System.out.println("coudlnt delete last element");
+        }
+
+        check = list.remove(3);
+        if(check){
+            success++;
+            System.out.println("deleted middle element");
+        }else{
+            fail++;
+            System.out.println("coudlnt delete middle elemnt");
+        }
+
+        list.remove(2);
+
+        check = list.remove(4);
+        if(check){
+            success++;
+            System.out.println("deleted last element");
+        }else{
+            fail++;
+            System.out.println("coudlnt delete last element");
+        }
+
+
+        check = list.remove(10);
+        System.out.println(check);
+        if(!check){
+            success++;
+            System.out.println("couldnt delete from empty list");
+        }else {
+            fail++;
+            System.out.println("somehow deleted from empty list???");
+        }
+
+        System.out.println(success+" success");
+        System.out.println(fail+" fail");
+
+    }
+    public static void testRemoveAll(){
+        LinkedList2 list = new LinkedList2();
+        for(int i = 0; i<5; i++){
+            list.addInTail(new Node(i%3));
+        }
+        list.addInTail(new Node(5));
+        for(int i = 0; i<5; i++){
+            list.addInTail(new Node(i+1));
+        }
+        int success = 0, fail = 0;
+        int head = list.head.value;
+        int headNext = list.head.next.value;
+        int tail = list.tail.value;
+        int tailPrev = list.tail.prev.value;
+
+
+        list.removeAll(9);
+        int head1 = list.head.value;
+        int head1Next = list.head.next.value;
+        int tail1 = list.tail.value;
+        int tail1Prev = list.tail.prev.value;
+        if(head == head1 && headNext == head1Next && tail == tail1 && tailPrev == tail1Prev){
+            success++;
+            System.out.println("Nothing happened");
+        }else{
+            fail++;
+            System.out.println("something bad happened");
+        }
+
+        list.removeAll(0);
+        int head2 = list.head.value;
+        int head2Next = list.head.next.value;
+        int tail2 = list.tail.value;
+        int tail2Prev = list.tail.prev.value;
+        if(head2 == head1Next && tail1 == tail2 && tail1Prev == tail2Prev){
+            success++;
+            System.out.println("deleted head element and new head = head.next");
+        }else{
+            fail++;
+            System.out.println("couldnt delete head element");
+        }
+
+        list.removeAll(5);
+        int head3 = list.head.value;
+        int head3Next = list.head.next.value;
+        int tail3 = list.tail.value;
+        int tail3Prev = list.tail.prev.value;
+        if(tail3 != tail2 && tail3 == tail2Prev){
+            success++;
+            System.out.println("deleted tail");
+        }else{
+            fail++;
+            System.out.println("couldnt delete tail");
+        }
+
+        list.removeAll(1);
+        int head4 = list.head.value;
+        int head4Next = list.head.next.value;
+        if(head4 != head3 && head4 == head3Next){
+            success++;
+            System.out.println("deleted head element");
+        }else {
+            fail++;
+            System.out.println("couldnt delete head element");
+        }
+
+        list.removeAll(2);
+        int head5 = list.head.value;
+        int tail5 = list.tail.value;
+        list.removeAll(3);
+        int head6 = list.head.value;
+        int tail6 = list.tail.value;
+        if(head6 == tail5 && tail6 == tail5){
+            success++;
+            System.out.println("delete head succes");
+        }else{
+            fail++;
+            System.out.println("delete head failed");
+        }
+
+        list.remove(10);
+        Node h = list.head;
+        Node t = list.tail;
+        if(list.head == null){
+            success++;
+            System.out.println("head null");
+        }else{
+            fail++;
+            System.out.println("???");
+        }
+        //deleting from empty list
+
+        for(int i = 0; i < 3;i++){
+            list.addInTail(new Node(1));
+        }
+        //list.log();
+        list.removeAll(1);
+        if(list.head == null && list.tail == null){
+            success++;
+            //System.out.println("deleted all list");
+        }else{
+            fail++;
+            //System.out.println("coudlnt delete all list");
+        }
+        //list.log();
+
+        System.out.println(success+" success");
+        System.out.println(fail+" fail");
+    }
+    public static void testInsert(){
+        LinkedList2 list = new LinkedList2();
+        int success = 0, fail = 0;
+
+        list.insertAfter(list.find(123), new Node (15));
+        list.log();
+        if(list.head!= null && list.tail != null){
+            success++;
+            System.out.println("insert into empty list");
+        }else{
+            fail++;
+            System.out.println("fail insert into empty list");
+        }
+        for(int i = 0; i<5; i++){
+            list.addInTail(new Node(i));
+        }
+
+        list.insertAfter(list.head, new Node (10));
+        list.log();
+        if(list.head.next.value == 10){
+            success++;
+            System.out.println("insert after head");
+        }else{
+            fail++;
+            System.out.println("insert after head failed");
+        }
+
+        list.insertAfter(list.find(123), new Node (20));
+        list.log();
+        if(list.tail.value == 20){
+            success++;
+            System.out.println("insert before head(null)");
+        }else{
+            fail++;
+            System.out.println("insert before head failed");
+        }
+
+        list.insertAfter(list.tail, new Node(25));
+        list.log();
+        if(list.tail.value == 25){
+            success++;
+            System.out.println("insert after tail");
+        }else{
+            fail++;
+            System.out.println("insert after tail failed");
+        }
+
+        int tail1 = list.tail.value;
+        list.insertAfter(list.tail.prev, new Node (30));
+        list.log();
+        if(tail1 == list.tail.value && list.tail.prev.value == 30){
+            success++;
+            System.out.println("insert before tail");
+        }else{
+            fail++;
+            System.out.println("insert before tail failed");
+        }
+
+        Node findMid = list.find(2);
+        Node midNext = findMid.next;
+        list.insertAfter(findMid, new Node(40));
+        list.log();
+        if(midNext!= findMid.next && findMid.next.value == 40){
+            success++;
+            System.out.println("insert in the middle of list");
+        }else{
+            fail++;
+            System.out.println("insert in the middle of list");
+        }
+
+        Node t = list.tail;
+        list.insertAfter(list.tail.next, new Node(45));
+        list.log();
+        if(list.tail.prev == t && list.tail.value == 45){
+            success++;
+            System.out.println("insert after tail.next of list");
+        }else{
+            fail++;
+            System.out.println("insert after tail.next of list");
+        }
+
+        System.out.println(success+" success");
+        System.out.println(fail+" fail");
+    }
+    public static void testFindAll(){
+        LinkedList2 list = new LinkedList2();
+        for(int i = 0; i< 5; i++){
+            list.addInTail(new Node(i));
+            list.addInTail(new Node(6-i));
+        }
+        list.log();
+        int count1 = 0;
+        ArrayList<Node> arrayList = list.findAll(1);
+        for(int i = 0; i<arrayList.size(); i++){
+            System.out.println(arrayList.get(i).value);
+        }
+
+
+    }
+}
+class Main{
+    public static void main(String[] args){
+        Test.testCount();
+        Test.testFind();
+        Test.testRemove();
+        Test.testRemoveAll();
+        Test.testInsert();
+        Test.testFindAll();
+
+
+    }
+}
+
+//list.head.value
+//list.head.value
